6cb06c1fd28a453f8556b49fbb74819427d71551
Mason Hall <mason@jmh.fyi>
2024-10-26 11:40:19 -0400

    Add some git aliases

--- shell/aliases-and-functions.sh
+++ shell/aliases-and-functions.sh
@@ -4,6 +4,10 @@ alias eeval='emacsclient --eval'
 alias eping='eeval "(+ 8 34)"'
 alias estop='eeval "(save-buffers-kill-emacs)"'
 alias ekill='eeval "(kill-emacs)"'
+# git
+alias gst='git status'
+alias gc='git commit'
+alias gd='git diff'
 # commands
 alias fd='fd --hidden'
 alias rg='rg --with-filename --no-heading --hidden'
