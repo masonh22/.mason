371ff4205fc8e103a03a00c0e4f12c54e91caceb
Mason Hall <mason@jmh.fyi>
2024-11-27 11:06:23 -0500

    Rework prompt.bash so it's extensible

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,7 +1,19 @@
-current_prompt_style=""
+# To add one or more prompts, write a function to add prompt strings to
+# [prompts] (and optionally bracket colors to [brackets]), then add this
+# generating function to prompt_gen.
 
-__PS1=${PS1}
-__BRACKET_DEFAULT='\e[0m' # no formatting
+# stores prompt strings
+declare -A prompts=()
+# stores bracket colors
+declare -A brackets=()
+# stores functions for generating prompts, i.e. adding strings to [prompts]
+# and [brackets]
+declare -a prompt_gen=()
+
+current_prompt_style=;
+
+__BRACKET_INIT='\e[0m' # no formatting
+__BRACKET_DEFAULT=${__BRACKET_INIT}
 
 # wrapper to grab the exit code of the last command
 __prompt_command() {
@@ -15,88 +27,99 @@ __prompt_command() {
     # add a "Operating System Command" for the current directory
     # https://www.gnu.org/software/emacs/manual/html_node/emacs/Interactive-Shell.html
     printf "\e]7;file://%s%s\e\\" "$HOSTNAME" "$PWD"
-
-    PS1=${__PS1}
 }
 PROMPT_COMMAND=__prompt_command
 
 function prompt() {
-    bold='\[\e[1m\]'
-    unbold='\[\e[21m\]'
-    clear_formatting='\[\e[0m\]'
-    black='\[\e[30m\]'
-    bright_black='\[\e[90m\]'
-    red='\[\e[31m\]'
-    bright_red='\[\e[91m\]'
-    green='\[\e[32m\]'
-    bright_green='\[\e[92m\]'
-    yellow='\[\e[33m\]'
-    bright_yellow='\[\e[93m\]'
-    blue='\[\e[34m\]'
-    bright_blue='\[\e[94m\]'
-    magenta='\[\e[35m\]'
-    bright_magenta='\[\e[95m\]'
-    cyan='\[\e[36m\]'
-    bright_cyan='\[\e[96m\]'
-    white='\[\e[37m\]'
-    bright_white='\[\e[97m\]'
+    if [ -z "$1" ]; then
+        if [ -z "${current_prompt_style}" ]; then
+            >&2 echo 'Cannot rebuild prompt with previous style the first time prompt is called.'
+            return 2
+        fi
+        echo "Regenerating prompt with style \"${current_prompt_style}\""
+        prompt ${current_prompt_style}
+        return
+    fi
 
-    username='\u'
-    histname='\h'
-    current_time='\t'
-    current_dir='\w'
+    for gen_func in "${prompt_gen[@]}"; do
+        $gen_func || return
+    done
 
+    local new_prompt="${prompts[$1]}"
+    if [ -z "$new_prompt" ]; then
+        >&2 echo "Unknown prompt preset '$1'"
+        return 1
+    fi
+    current_prompt_style="$1"
+    PS1="$new_prompt"
+    local new_bracket="${brackets[$1]}"
+    if [ -n "$new_bracket" ]; then
+        __BRACKET_DEFAULT="$new_bracket"
+    else
+        __BRACKET_DEFAULT="${__BRACKET_INIT}"
+    fi
+}
+function _prompt_list() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "${!prompts[*]}" $cur) )
+}
+complete -o default -F _prompt_list prompt
+
+# useful things to include in prompts
+gen_prompt_utils() {
     fancy_hostname="$(if [ -n "${IS_SSH}" ]; then echo "$(color_text @$(hostname | cut -d "." -f 1))"; fi)"
     opam_switch='$(color_text "($(opam switch show))")'
     fancy_username="$(color_text $(whoami))"
     fancy_cursor='\e[?6;0;13;c'
     # change the bracket color depending on whether the last command failed
     bracket_color='if [ "$__PROMPT_EXIT" = "0" ]; then echo -e "${__BRACKET_DEFAULT}"; else echo -e "\e[0m\e[31m"; fi'
-
-    if [ -z "$1" ]; then
-        if [ -z "${current_prompt_style}" ]; then
-            >&2 echo "Cannot rebuild prompt with previous style the first time build_prompt is called."
-            return 2
-        fi
-        echo "Regenerating prompt with style \"${current_prompt_style}\""
-        build_prompt ${current_prompt_style}
-        return
-    fi
-
-    case "$1" in
-        colorless)
-            __PS1="[${username} ${current_dir}] \$ "
-            ;;
-        simple)
-            __PS1="[${fancy_username} ${current_dir}]\n\$ "
-            ;;
-        git|default|mason)
-            __BRACKET_DEFAULT='\e[0m' # no formatting
-            __PS1="\$(${bracket_color})[${bright_magenta}${current_time} ${fancy_username}${fancy_hostname} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
-            ;;
-        opam|ocaml)
-            __BRACKET_DEFAULT='\e[90m' # bright black
-            __PS1="\$(${bracket_color})[${bright_magenta}${current_time} ${opam_switch} ${fancy_username} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
-            ;;
-        full)
-            # same as opam but with hostname
-            __BRACKET_DEFAULT='\e[90m' # bright black
-            __PS1="\$(${bracket_color})[${bright_magenta}${current_time} ${opam_switch} ${fancy_username}${fancy_hostname} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
-            ;;
-        *)
-            >&2 echo "Unknown prompt preset \"$1\""
-            return 1
-            ;;
-    esac
-
-    current_promt_style=$1
 }
-function _prompt_list() {
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    export prompts="colorless simple default ocaml full"
-    COMPREPLY=( $(compgen -W "$prompts" $cur) )
+prompt_gen+=(gen_prompt_utils)
+
+prompt_mason() {
+    local bold='\[\e[1m\]'
+    local unbold='\[\e[21m\]'
+    local clear_formatting='\[\e[0m\]'
+    local black='\[\e[30m\]'
+    local bright_black='\[\e[90m\]'
+    local red='\[\e[31m\]'
+    local bright_red='\[\e[91m\]'
+    local green='\[\e[32m\]'
+    local bright_green='\[\e[92m\]'
+    local yellow='\[\e[33m\]'
+    local bright_yellow='\[\e[93m\]'
+    local blue='\[\e[34m\]'
+    local bright_blue='\[\e[94m\]'
+    local magenta='\[\e[35m\]'
+    local bright_magenta='\[\e[95m\]'
+    local cyan='\[\e[36m\]'
+    local bright_cyan='\[\e[96m\]'
+    local white='\[\e[37m\]'
+    local bright_white='\[\e[97m\]'
+
+    local username='\u'
+    local histname='\h'
+    local current_time='\t'
+    local current_dir='\w'
+
+    prompts['colorless']="[${username} ${current_dir}] \$ "
+    prompts['minimal']="${prompts['colorless']}"
+
+    prompts['simple']="[${fancy_username} ${current_dir}]\n\$ "
+
+    prompts['normal']="\$(${bracket_color})[${bright_magenta}${current_time} ${bright_cyan}${fancy_username}${fancy_hostname} ${bright_cyan}${current_dir}\$(${bracket_color})]${clear_formatting}\n\$ "
+
+    prompts['mason']="\$(${bracket_color})[${bright_magenta}${current_time} ${fancy_username}${fancy_hostname} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
+    prompts['git']="${prompts['mason']}"
+    prompts['default']="${prompts['mason']}"
+
+    prompts['ocaml']="\$(${bracket_color})[${bright_magenta}${current_time} ${opam_switch} ${fancy_username} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
+
+    # same as opam but with hostname
+    prompts['full']="\$(${bracket_color})[${bright_magenta}${current_time} ${opam_switch} ${fancy_username}${fancy_hostname} ${bright_cyan}${current_dir}\$(git_prompt)\$(${bracket_color})]${clear_formatting}\n\$ "
+    brackets['full']='\e[90m' # bright black
 }
-complete -o default -F _prompt_list prompt
+prompt_gen+=(prompt_mason)
 
 if [ -x "$(command -v git)" ]; then
     prompt git > /dev/null
