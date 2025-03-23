327453ee158f5f469f2e4fb3e84d2b2cd4a963ce
Mason Hall <mason@jmh.fyi>
2025-03-23 11:43:02 -0400

    prot-inspired avoid initial flash of light
    commented out because I'm not sure I'm getting this anymore?

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -2,3 +2,13 @@
 
 ;; Disable package.el for straight.el
 ;; (setq package-enable-at-startup nil)
+
+(defun mason-avoid-initial-flash-of-light ()
+  "Avoid flash of light when starting Emacs"
+  (progn
+    (setq mode-line-format nil)
+    (set-face-attribute 'default nil :background "#000000" :foreground "#ffffff")
+    (set-face-attribute 'mode-line nil :background "#000000" :foreground "#ffffff" :box 'unspecified)
+    ))
+
+;; (mason-avoid-initial-flash-of-light)
