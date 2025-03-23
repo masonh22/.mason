aa3ff72bd68c9e6c4b1cecb047b8a0d4293af39f
Mason Hall <mason@jmh.fyi>
2025-03-23 12:25:46 -0400

    Add scripts/check-tools.bash

---
+++ scripts/check-tools.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# This checks for whether common tools I use are installed. Ansible would
+# probably be better for this, but it's pretty heavy-duty.
+
+essentials=(
+    curl
+    emacs
+    git
+    htop
+)
+
+extras=(
+    batcat
+    fd
+    rg
+)
+
+languages=(
+    python3
+    pip3
+
+    opam
+
+    rustup
+    cargo
+    rustc
+)
+
+ret=0
+
+for program in "${essentials[@]}" "${extras[@]}" "${languages[@]}"; do
+    if [ ! -x "$(command -v "$program")" ]; then
+        echo "Missing '$program'" >&2
+        ret=1
+    fi
+done
+
+exit $ret
