c577de9d3306b1eb309de406b96aae88da7dfe9a
Mason Hall <mason@jmh.fyi>
2025-04-05 23:28:39 -0400

    disable bell in zsh

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -4,6 +4,7 @@ set -o SHARE_HISTORY # don't overwrite hist files on exit
 set -o APPEND_HISTORY # append to hist file TODO diff between this and prev opt?
 set -o HIST_FIND_NO_DUPS # ignore dups when searching
 set -o HIST_REDUCE_BLANKS # don't store blank lines
+set +o BEEP # disable bell
 
 setopt prompt_subst # Enable prompt substitution
 
