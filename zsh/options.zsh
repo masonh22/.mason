5706f3079dae04baf800b29ea231f8c562ea00c4
Mason Hall <mason@jmh.fyi>
2025-06-26 19:45:30 -0400

    don't share zsh history between live sessions
    it's annoying

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,7 +1,7 @@
 set -o GLOB_COMPLETE # don't substitute glob patterns, but generate completions
 set -o EXTENDED_HISTORY # timestamp/runtime in hist file
-set -o SHARE_HISTORY # don't overwrite hist files on exit
-set -o APPEND_HISTORY # append to hist file TODO diff between this and prev opt?
+set -o APPEND_HISTORY # append to hist file
+set -o INC_APPEND_HISTORY_TIME # write to the hist file after commands finish
 set -o HIST_FIND_NO_DUPS # ignore dups when searching
 set -o HIST_REDUCE_BLANKS # don't store blank lines
 set -o HIST_IGNORE_DUPS # ignore consecutive commands in history
