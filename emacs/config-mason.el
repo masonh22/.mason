310d6b13d679893eab965e20dba1eb755255efaf
Mason Hall <mason@jmh.fyi>
2025-09-05 16:56:22 -0400

    set emacs font explicitly

--- emacs/config-mason.el
+++ emacs/config-mason.el
@@ -17,6 +17,11 @@
   (scroll-bar-mode -1))
 (menu-bar-mode -1)
 
+(when (display-graphic-p)
+  (set-face-attribute 'default nil
+                      :family "monospace"
+                      :height 130))
+
 ;; use utf-8 in terminals
 (set-terminal-coding-system 'utf-8)
 
