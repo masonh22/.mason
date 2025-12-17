e306a1599579d448516c8c044806bf5a71797b0e
Mason Hall <mason@jmh.fyi>
2025-12-16 22:26:52 -0500

    add zsh to check-tools and check whether it's the default shell

--- scripts/check-tools.bash
+++ scripts/check-tools.bash
@@ -9,6 +9,7 @@ essentials=(
     emacs
     git
     htop
+    zsh
 )
 
 extras=(
@@ -38,5 +39,12 @@ for program in "${essentials[@]}" "${extras[@]}" "${languages[@]}"; do
         ret=1
     fi
 done
+
+# Check whether zsh is the default shell
+if [ "$(basename $SHELL)"  != "zsh" ]; then
+    echo "Default shell is $(basename $SHELL), change it to zsh with:" >&2
+    echo '  chsh -s $(which zsh)' >&2
+    ret=2
+fi
 
 exit $ret
